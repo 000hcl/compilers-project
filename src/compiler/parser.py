640a0fa0eae9b73bf829623a5e7284b2d99bf9a1
Natalie Johansson <mnrjohansson@gmail.com>
2024-02-20 19:38:56 +0200

    parser left and right assoc

--- src/compiler/parser.py
+++ src/compiler/parser.py
@@ -18,22 +18,22 @@ def parse(tokens: list[Token]) -> ast.Expression:
     def consume(expected: str | list[str] | None = None) -> Token:
         token = peek()
         if isinstance(expected, str) and token.text != expected:
-            raise Exception(f'{token.location}: expected "{expected}"')
+            raise Exception(f'{token.loc}: expected "{expected}"')
         if isinstance(expected, list) and token.text not in expected:
             comma_separated = ", ".join([f'"{e}"' for e in expected])
-            raise Exception(f'{token.location}: expected one of: {comma_separated}')
+            raise Exception(f'{token.loc}: expected one of: {comma_separated}')
         pos += 1
         return token
     
     def parse_int_literal() -> ast.Literal:
         if peek().type != 'int_literal':
-            raise Exception(f'{peek().location}: expected an integer literal')
+            raise Exception(f'{peek().loc}: expected an integer literal')
         token = consume()
         return ast.Literal(int(token.text))
     
     def parse_identifier() -> ast.Identifier:
         if peek().type != 'identifier':
-            raise Exception(f'{peek().location}: expected an identifier')
+            raise Exception(f'{peek().loc}: expected an identifier')
         token = consume()
         return ast.Identifier(token.text)
     
@@ -43,16 +43,52 @@ def parse(tokens: list[Token]) -> ast.Expression:
         elif peek().type == 'identifier':
             return parse_identifier()
         else:
-            raise Exception(f'{peek().location}: expected an integer literal or an identifier')
+            raise Exception(f'{peek().loc}: expected an integer literal or an identifier')
     
     def parse_expression() -> ast.BinaryOp:
         left = parse_term()
-        operator_token = consume(['+','-'])
-        right = parse_term()
-        return ast.BinaryOp(
-            left,
-            operator_token.text,
-            right
-        )
+        
+        while peek().text in ['+','-']:
+            operator_token = consume()
+            operator = operator_token.text
+            
+            right = parse_term()
+            left = ast.BinaryOp(
+                left,
+                operator,
+                right
+            )
+        
+        return left
+    
+    #TODO: testing
+    def parse_expression_right() -> ast.BinaryOp:
+        pos_copy = pos
+        #get all relevant tokens
+        expression_tokens = [consume()]
+        while peek().text in ['+','-']:
+            #consume operator
+            expression_tokens.append(consume())
+            #consume right integer
+            expression_tokens.append(consume())
+            
+        
+        #reverse list
+        expression_tokens = expression_tokens[::-1]
+        #parse
+        right = parse_term(expression_tokens[0])
+        for tkn in expression_tokens[1:]:
+            if tkn.text in ['+','-']:
+                op_token = tkn.text
+            else:
+                left = parse_term(tkn)
+                right = ast.BinaryOp(
+                    left,
+                    op_token,
+                    right
+                )
+        return right
+            
+    
     return parse_expression()
 
